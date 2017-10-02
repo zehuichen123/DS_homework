978cd7d89adcbda94b55017bab14c83eca322747
kirito <kirito@kiritodeMacBook-Pro.local>
2017-10-02 11:26:13 +0800

    homework2 finished

--- homework2.cpp
+++ homework2.cpp
@@ -8,13 +8,13 @@ public:
 public:
     int number;
     Person* next;
-    Person* former;
 };
 class People{
 public:
     People();
     ~People(){};
     void Game();
+    bool Check();
 
 public:
     int N;
@@ -22,44 +22,96 @@ public:
     int M;
     int K;
     Person* Head;
+    Person* Rear;
 };
+bool People::Check(){
+    if(N<=0){
+        cout<<"The total number N should be positive!"
+                "Please REINPUT YOUR DATA!"<<endl;
+        return false;
+    }
+    else if(S<=0){
+        cout<<"The begin point S should be positive!"
+                "Please REINPUT YOUR DATA!"<<endl;
+        return false;
+    }
+    else if(M<=0){
+        cout<<"The dead number M should be positive!"
+                "Please REINPUT YOUR DATA!"<<endl;
+        return false;
+    }
+    else if(K<=0){
+        cout<<"The residual number K should be positive!"
+                "Please REINPUT YOUR DATA!"<<endl;
+        return false;
+    }
+    else if(S>N){
+        cout<<"The begin point S should less than "
+                "the total number N!"
+                "Please REINPUT YOUR DATA!"<<endl;
+        return false;
+    }
+    else if(K>N){
+        cout<<"The residual number K must less than "
+                "the total number N!"
+                "Please REINPUT YOUR DATA!"<<endl;
+        return false;
+    }
+    else{
+        return true;
+    }
+}
 People::People() {
-    cout<<"请输入生死游戏的总人数N:";
-    cin>>N;
-    cout<<"请输入游戏开始的位置S:";
-    cin>>S;
-    cout<<"请输入死亡数字M:";
-    cin>>M;
-    cout<<"请输入剩余的生者人数K:";
-    cin>>K;
+    cout<<"现有N个人围成一圈，从第S个人开始依次报数，"
+            "报M的人出局，再由下一个人开始报数。如此"
+            "循环，直至只剩下K个人为止"<<endl;
+    bool check;
+    do{
+        cout<<"请输入生死游戏的总人数N:";
+        cin>>N;
+        cout<<"请输入游戏开始的位置S:";
+        cin>>S;
+        cout<<"请输入死亡数字M:";
+        cin>>M;
+        cout<<"请输入剩余的生者人数K:";
+        cin>>K;
+        check=Check();
+    }while(!check);
     auto ptr1=new Person;
     ptr1->number=1;
     Head=ptr1;
     for(int i=2;i<=N;i++){
         auto ptr2=new Person;
         ptr2->number=i;
-        ptr2->former=ptr1;
         ptr1->next=ptr2;
         if(i==N){
             ptr2->next=Head;
-            Head->former=ptr2;
+            Rear=ptr2;
         }
         ptr1=ptr2;
     }
 }
 void People::Game(){
     auto ptr=Head;
+    auto ptrFormer=Head;
+    if(S==1){
+        ptrFormer=Rear;
+    }
+    else{
+        for(int i=0;i<S-2;i++){
+            ptrFormer=ptrFormer->next;
+        }
+    }
     for(int i=0;i<S-1;i++){
         ptr=ptr->next;
     }
     for(int i=1;i<=N-K;i++){
         for(int j=0;j<M-1;j++){
             ptr=ptr->next;
+            ptrFormer=ptrFormer->next;
         }
-        auto ptrFormer=ptr->former;
         auto ptrNext=ptr->next;
         ptrFormer->next=ptrNext;
-        ptrNext->former=ptrFormer;
         cout<<"第"<<i<<"个死者的位置是："<<(ptr->number)<<endl;
         if(ptr==Head){
             Head=ptr->next;
@@ -80,5 +132,6 @@ int main(void){
     People system1;
     system1.Game();
     cout<<"press any key to continue:";
+    getchar();
     getchar();
 }
