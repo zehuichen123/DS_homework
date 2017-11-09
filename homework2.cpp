76a30ebeaa7122e203eb3f9315498d9affe8fc46
loveSnowBest <lovesnowbest@gmail.com>
2017-11-09 16:28:14 +0800

    add notations and document to homework2

--- homework2.cpp
+++ homework2.cpp
@@ -1,27 +1,26 @@
 #include<iostream>
 using namespace std;
-class Person{
+class Person{                           
 public:
     Person(){}
     ~Person(){}
-
 public:
-    int number;
-    Person* next;
+    int number;                         //每个人的序号
+    Person* next;                       //指向下一个人
 };
-class People{
+class People{                           
 public:
     People();
     ~People(){};
-    void Game();
-    bool Check();
+    void Game();                        //开始进行模拟生死游戏
+    bool Check();                       //检查初始输入是否合理
 
 public:
-    int N;
-    int S;
-    int M;
-    int K;
-    Person* Head;
+    int N;                              //开始时参与该游戏总人数
+    int S;                              //开始时从第S个人开始模拟
+    int M;                              //每数到第M个人就让该人出列
+    int K;                              //最后剩余K人时输出剩余人信息
+    Person* Head;                       
     Person* Rear;
 };
 bool People::Check(){
@@ -77,6 +76,7 @@ People::People() {
         cin>>K;
         check=Check();
     }while(!check);
+    /*构造一个循环单链表*/
     auto ptr1=new Person;
     ptr1->number=1;
     Head=ptr1;
@@ -93,31 +93,31 @@ People::People() {
 }
 void People::Game(){
     auto ptr=Head;
-    auto ptrFormer=Head;
-    if(S==1){
+    auto ptrFormer=Head;            //指向当前位置的前一个人
+    if(S==1){                       //如果当前初始位置为1，那么前一个人就是最后一个
         ptrFormer=Rear;
     }
-    else{
+    else{                           //如果不是就顺序遍历到第S-1位置
         for(int i=0;i<S-2;i++){
             ptrFormer=ptrFormer->next;
         }
     }
-    for(int i=0;i<S-1;i++){
+    for(int i=0;i<S-1;i++){         //将ptr指向初始位置也就是S位置
         ptr=ptr->next;
     }
-    for(int i=1;i<=N-K;i++){
-        for(int j=0;j<M-1;j++){
+    for(int i=1;i<=N-K;i++){        //进行N-K次模拟删除节点
+        for(int j=0;j<M-1;j++){     //模拟数到第M个人(将ptr和ptrFormer一起迭代)
             ptr=ptr->next;
             ptrFormer=ptrFormer->next;
         }
-        auto ptrNext=ptr->next;
-        ptrFormer->next=ptrNext;
+        auto ptrNext=ptr->next;     //找到当前位置的下一个元素
+        ptrFormer->next=ptrNext;    //将当前位置的前一个元素链接至当前位置的下一个元素
         cout<<"第"<<i<<"个死者的位置是："<<(ptr->number)<<endl;
-        if(ptr==Head){
+        if(ptr==Head){              //如果当前位置为头节点就更新头节点的值（因为最后输出时要用到head）
             Head=ptr->next;
         }
-        delete(ptr);
-        ptr=ptrNext;
+        delete(ptr);                //删除该位置节点
+        ptr=ptrNext;                //继续模拟操作
     }
     cout<<"最后剩下"<<K<<"人"<<endl;
     cout<<"剩余的生者位置为";
