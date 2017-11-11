e3289612b8d97a6ffde6caee518bfb8d9853eb3b
loveSnowBest <lovesnowbest@gmail.com>
2017-11-11 14:08:09 +0800

    add notations and document to homework5

--- homework5.cpp
+++ homework5.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 #include <fstream>
 using namespace std;
-ifstream openFile;
-bool checkOption(){
+ifstream openFile;               //定义文件输出流
+bool checkOption(){              //询问用户是否想重新输入
     cout<<"Do you want to reinput?(y/n)"<<endl;
     char option;
     cin>>option;
@@ -16,11 +16,11 @@ bool checkOption(){
 void Count(){
     int countNum=0;
     cout<<"please input the word you want to count:";
-    string word;
+    string word;                 //保存输入的单词
     cin>>word;
-    string temp;
+    string temp;                 //保存从文本中每次读入的单词
     while(openFile>>temp){
-        if(temp==word){
+        if(temp==word){          //每次出现一次就将计数器加1
             countNum++;
         }
     }
@@ -28,17 +28,20 @@ void Count(){
 }
 void Find(){
     cout<<"please input the word you want to count:";
-    string word;
+    string word;                 //保存想要搜索的单词
     cin>>word;
     int lineNum=1,pos=0;
     string line;
-    while(getline(openFile,line)){
-        while((pos=line.find(word,pos))!=string::npos){
+    while(getline(openFile,line)){      //每次读入一行
+        while((pos=line.find(word,pos))!=string::npos
+            &&(line[pos+word.size()]==' '||line[pos+word.size()]=='\n')){     
+                                                            //在每一行中重复寻找是否存在该单词
+                                                            //pos代表当前行寻找到的位置
             cout<<"find word "<<'\''<<word<<'\''<<" at line "<<lineNum<<",column "<<pos<<endl;
             pos+=word.size();
         }
         lineNum++;
-        pos=0;
+        pos=0;                          //将每一行寻找到的位置重置为0                  
     }
 }
 int main(void){
@@ -48,12 +51,12 @@ int main(void){
         string fileName;
         cin>>fileName;
         openFile.open(fileName);
-        if(openFile.is_open()){
+        if(openFile.is_open()){         //如果文件正常打开就执行操作
             bool check1=1;
             while(check1){
                 //getchar();
                 openFile.clear();
-                openFile.seekg(0,ios::beg);
+                openFile.seekg(0,ios::beg);     //每次进行使用时将读入指针重置为首位
                 char option;
                 cout<<"------------------------"<<endl;
                 cout<<"选项1：对给定单词计数"<<endl;
