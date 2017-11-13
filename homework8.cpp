9d9602728a715d56c3b0e4a7af86dbe864667847
loveSnowBest <lovesnowbest@gmail.com>
2017-11-13 16:22:19 +0800

    add notation and documents to homework8

--- homework8.cpp
+++ homework8.cpp
@@ -1,22 +1,22 @@
 #include <iostream>
 #include <vector>
-#define INF 0x7fffffff
+#define INF 0x7fffffff              //定义无穷大数为SIGNED INT的MAX
 using namespace std;
-class Graph{
+class Graph{                        //定义图结构
 public:
     Graph();
     ~Graph(){};
-    void Process();
-    void CreateE();
-    void CreateV();
-    void Prim();
-    void ShowTree();
-    int getIndex(char name);
+    void Process();                 //读入用户输入选项
+    void CreateE();                 //创建电网的边
+    void CreateV();                 //创建电网的顶点
+    void Prim();                    //最小生成树算法
+    void ShowTree();                //展示最小生成树
+    int getIndex(char name);        //根据name获得树的节点
 private:
-    vector<char>primTree;
-    int map[1000][1000];
-    char char2Index[1000];
-    int vNum;
+    vector<char>primTree;           //储存最小树的路径
+    int map[1000][1000];            //使用邻接表保存各个顶点之间的连接情况
+    char char2Index[1000];          //保存从顶点映射成数字的规则
+    int vNum;                       //保存所有顶点的个数
 };
 Graph::Graph() {
     cout<<"**                  电网造价模拟系统               **"<<endl;
@@ -30,20 +30,20 @@ Graph::Graph() {
 }
 
 int Graph::getIndex(char name) {
-    for(int i=0;i<vNum;i++){
+    for(int i=0;i<vNum;i++){           //从保存的节点中找到name的节点并返回对应的数字
         if(char2Index[i]==name){
             return i;
         }
     }
-    return -1;
+    return -1;                         //如果没有找到名字为name的顶点就返回-1
 }
 void Graph::CreateV() {
     cout<<"请输入顶点的个数：";
     cin>>vNum;
     cout<<"请依次输入各顶点的名称：";
     getchar();
-    for(int i=0;i<vNum;i++){
-        char2Index[i]=getchar();
+    for(int i=0;i<vNum;i++){           //依次获得顶点的名称并将其对应成数字
+        char2Index[i]=getchar();       //映射关系保存在char2Index数组中
         getchar();
     }
     cout<<endl;
@@ -51,10 +51,10 @@ void Graph::CreateV() {
 void Graph::CreateE() {
     for (int i = 0; i < 1000; ++i) {
         for (int j = 0; j < 1000; ++j) {
-            map[i][j]=INF;
+            map[i][j]=INF;             //这里将两个顶点没有通路时其权值为INF
         }
     }
-    for(int i=0;i<vNum*(vNum-1)/2;i++){
+    for(int i=0;i<vNum*(vNum-1)/2;i++){   //有vNum个顶点所以最多有vNum*(vNum-1)/2条边
         cout<<"请输入两个顶点及边：";
         char vertex1,vertex2;
         getchar();
@@ -62,27 +62,27 @@ void Graph::CreateE() {
         getchar();
         vertex2=getchar();
         cout<<vertex1<<" "<<vertex2<<endl;
-        int index1=getIndex(vertex1),index2=getIndex(vertex2);
-        if(index1<0||index2<0){
+        int index1=getIndex(vertex1),index2=getIndex(vertex2);  //获得顶点对应的数字
+        if(index1<0||index2<0){        //检查用户输入是否合法
             cout<<"the vertex doesn't exist! Please reinput!"<<endl;
             i--;
             continue;
         }
         else{
             cin>>map[index1][index2];
-            map[index2][index1]=map[index1][index2];
+            map[index2][index1]=map[index1][index2];       //邻接矩阵为对称
         }
     }
 }
 void Graph::Prim() {
-    while(!primTree.empty())primTree.pop_back();     //清空原先prim树
-    bool visited[vNum];
+    while(!primTree.empty())primTree.pop_back();    //清空原先prim树
+    bool visited[vNum];                             //用于保存该顶点是否已经被连接入树
     memset(visited,false,vNum);
     cout<<"请输入起始顶点:";
-    char start;
+    char start;                                     //最小树开始的起点
     cin>>start;
-    primTree.push_back(start);
-    for(int i=0;i<vNum-1;i++){
+    primTree.push_back(start);                      //将起点放入primTree中
+    for(int i=0;i<vNum-1;i++){                      //从所有边中找出权值最小的边的另一个顶点
         int vertex=getIndex(primTree.back());
         int vMin=INF,temp;
         for(int i=0;i<vNum;i++){
@@ -91,8 +91,8 @@ void Graph::Prim() {
                 temp=i;
             }
         }
-        visited[temp]=true;
-        primTree.push_back(char2Index[temp]);
+        visited[temp]=true;                         //将该顶点设置为已经被并入树中
+        primTree.push_back(char2Index[temp]);       //将该顶点放入primTree中作为最后答案
     }
 }
 void Graph::ShowTree() {
